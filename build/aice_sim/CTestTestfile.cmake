6d750252b761990640f6f2849d6a4a7552096496
Dion Jose <dj3g24@soton.ac.uk>
2026-03-20 21:23:09 +0000

    Delete build directory

--- build/aice_sim/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aice/ros2_ws/src/turtlebot3_task
-# Build directory: /home/aice/ros2_ws/build/aice_sim
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
